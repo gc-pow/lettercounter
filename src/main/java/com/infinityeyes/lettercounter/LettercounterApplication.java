eb2ab2f06f889d0c3c23be29c705853914d186ca
pow <aleksandar.coha@gmail.com>
2018-04-30 14:24:44 +0200

    Initial commit

---
+++ src/main/java/com/infinityeyes/lettercounter/LettercounterApplication.java
@@ -0,0 +1,12 @@
+package com.infinityeyes.lettercounter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LettercounterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LettercounterApplication.class, args);
+	}
+}
